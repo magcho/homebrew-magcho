b782c71e3ba442c8658f39d6ee4d0163e7b689a5
magcho <mail@magcho.com>
2020-02-24 00:32:32 +0900

    [fix] dotz formula audit fix

--- dotz.rb
+++ dotz.rb
@@ -1,8 +1,8 @@
 class Dotz < Formula
   desc "backup and restore dotfiles"
   homepage "https://github.com/magcho/dotz/"
-  version "0.1.1"
   url "https://github.com/magcho/dotz/releases/download/v#{version}/dotz"
+  version "0.1.1"
   sha256 "c9f628993cb20abb0c1f8553602d7f87ed86f9b8b123b97bfd1a4b84585e7c56"
 
   def install
