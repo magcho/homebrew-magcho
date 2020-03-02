4f4c84e11d4a2060219227837dd162e57dda3acd
magcho <mail@magcho.com>
2020-03-02 12:44:44 +0000

    update dotz

--- dotz.rb
+++ dotz.rb
@@ -1,8 +1,8 @@
 class Dotz < Formula
   desc "backup and restore dotfiles"
   homepage "https://github.com/magcho/dotz/"
-  url "https://github.com/magcho/dotz/releases/download/v0.1.1/dotz"
-  sha256 "c9f628993cb20abb0c1f8553602d7f87ed86f9b8b123b97bfd1a4b84585e7c56"
+  url "https://github.com/magcho/dotz/releases/download/v0.2.0/dotz"
+  sha256 "496c0b129cf430974e1327b0cbca51818c9d3cf02e40514c3b845aed46655819"
 
   def install
     bin.install "dotz"
