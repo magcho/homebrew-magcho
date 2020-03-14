5c28d7f5eafee6df65927e47271feafb5f629969
magcho <mail@magcho.com>
2020-03-14 22:27:41 +0900

    [add] ricoh theta

---
+++ Casks/ricoh-theta.rb
@@ -0,0 +1,12 @@
+cask 'ricoh-theta' do
+  version '3.12.0'
+  sha256 '820d1b6be34aa127a723c01ba5abf4247aeb2d58f80a3157f9b449eb0bff164b'
+
+  url 'https://theta360.com/intl/support/download/pcapp/macosx'
+  name 'RICOH THETA'
+  homepage 'https://theta360.com/ja/'
+
+  app 'RICOH THETA.app'
+
+  uninstall pkgutil: "com.ricoh.thetasphericalviewer"
+end
